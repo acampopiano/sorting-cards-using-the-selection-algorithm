5841c0f1f47122c6ddeeb8dfe770daea5a77eff2
Andres Campopiano <acampopiano@gmail.com>
2021-04-23 16:42:44 +0000

    five commit

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ window.onload = function() {
   for (var i = 0, l = matches.length; i < l; i++)
     result.push(matches[i].outerHTML);
 
-  result = result.sort(() => Math.random() - 0.5);
-
+  //result = result.sort(() => Math.floor(Math.random() * result.length));
+  shuffle(result);
   const inputCards = document.querySelector('input[name="cardsToDraw"]');
   var numberOfCards = inputCards.value || 0;
   inputCards.addEventListener("change", function(e) {
@@ -24,36 +24,27 @@ window.onload = function() {
       deck.innerHTML = "";
       for (let i = 0; i < numberOfCards; i++) {
         let mf = Math.floor(Math.random() * result.length);
-        deck.innerHTML += result[i];
+        deck.innerHTML += result[mf + i];
       }
     }
   });
 
-  var contains = function(needle) {
-    // Per spec, the way to identify NaN is that it is not equal to itself
-    var findNaN = needle !== needle;
-    var indexOf;
+  function shuffle(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
 
-    if (!findNaN && typeof Array.prototype.indexOf === "function") {
-      indexOf = Array.prototype.indexOf;
-    } else {
-      indexOf = function(needle) {
-        var i = -1,
-          index = -1;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-        for (i = 0; i < this.length; i++) {
-          var item = this[i];
-
-          if ((findNaN && item !== item) || item === needle) {
-            index = i;
-            break;
-          }
-        }
-
-        return index;
-      };
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
     }
-
-    return indexOf.call(this, needle) > -1;
-  };
+    return array;
+  }
 };
