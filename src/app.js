f81244ee4e004a334451a8d1b608c92fab810bd7
Andres Campopiano <acampopiano@gmail.com>
2021-04-23 19:01:42 +0000

    subo cambios

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ window.onload = function() {
     result.push(matches[i].outerHTML);
 
   //result = result.sort(() => Math.floor(Math.random() * result.length));
-  shuffle(result);
+  //shuffle(result);
   const inputCards = document.querySelector('input[name="cardsToDraw"]');
   var numberOfCards = inputCards.value || 0;
   inputCards.addEventListener("change", function(e) {
@@ -22,13 +22,44 @@ window.onload = function() {
   btnDraw.addEventListener("click", function() {
     if (numberOfCards >= 0 && numberOfCards <= 52) {
       deck.innerHTML = "";
+      positions = [];
       for (let i = 0; i < numberOfCards; i++) {
         let mf = Math.floor(Math.random() * result.length);
-        deck.innerHTML += result[mf + i];
+        if (!contains.call(positions, mf)) {
+          deck.innerHTML += result[mf];
+          positions.push(mf);
+        } else --i;
       }
     }
   });
 
+  var contains = function(needle) {
+    // Per spec, the way to identify NaN is that it is not equal to itself
+    var findNaN = needle !== needle;
+    var indexOf;
+
+    if (!findNaN && typeof Array.prototype.indexOf === "function") {
+      indexOf = Array.prototype.indexOf;
+    } else {
+      indexOf = function(needle) {
+        var i = -1,
+          index = -1;
+
+        for (i = 0; i < this.length; i++) {
+          var item = this[i];
+
+          if ((findNaN && item !== item) || item === needle) {
+            index = i;
+            break;
+          }
+        }
+
+        return index;
+      };
+    }
+
+    return indexOf.call(this, needle) > -1;
+  };
   function shuffle(array) {
     var currentIndex = array.length,
       temporaryValue,
