bd1a95598547a21ede12825c53436c20c938b7ce
Andres Campopiano <acampopiano@gmail.com>
2021-04-23 14:54:17 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -4,21 +4,24 @@ import "./style.css";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  /*var result = [],
+  var result = [],
     matches = document.querySelectorAll("#vCard .card");
   for (var i = 0, l = matches.length; i < l; i++)
     result.push(matches[i].outerHTML);
 
-  let btn = document.getElementById("deck");
-
-  btn.addEventListener("click", function() {
-    document.getElementById("deck").innerHTML =
-      result[Math.floor(Math.random() * result.length)];
-  });*/
-  var numberOfCards = document.getElementById("cardsToDraw").value;
+  const inputCards = document.querySelector('input[name="cardsToDraw"]');
+  var numberOfCards = inputCards.value || 0;
+  inputCards.addEventListener("change", function(e) {
+    numberOfCards = this.value;
+  });
   let btnDraw = document.getElementById("btnDraw");
+  let deck = document.getElementById("deck");
   btnDraw.addEventListener("click", function() {
-    if (numberOfCards > 0) {
+    if (numberOfCards >= 0 && numberOfCards <= 52) {
+      deck.innerHTML = "";
+      for (let i = 0; i < numberOfCards; i++) {
+        deck.innerHTML += result[Math.floor(Math.random() * result.length)];
+      }
     }
   });
 };
