a88dad3d7e3ccaa9ab1a3e84b5ace0f263051486
Andres Campopiano <acampopiano@gmail.com>
2021-04-23 15:32:37 +0000

    third commit

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ import "./style.css";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  var result = [],
+  var positions = [],
+    result = [],
     matches = document.querySelectorAll("#vCard .card");
   for (var i = 0, l = matches.length; i < l; i++)
     result.push(matches[i].outerHTML);
@@ -20,8 +21,39 @@ window.onload = function() {
     if (numberOfCards >= 0 && numberOfCards <= 52) {
       deck.innerHTML = "";
       for (let i = 0; i < numberOfCards; i++) {
-        deck.innerHTML += result[Math.floor(Math.random() * result.length)];
+        let mf = Math.floor(Math.random() * result.length);
+        deck.innerHTML += contains.call(positions, mf)
+          ? positions.push(mf)
+          : result[mf];
       }
     }
   });
+
+  var contains = function(needle) {
+    // Per spec, the way to identify NaN is that it is not equal to itself
+    var findNaN = needle !== needle;
+    var indexOf;
+
+    if (!findNaN && typeof Array.prototype.indexOf === "function") {
+      indexOf = Array.prototype.indexOf;
+    } else {
+      indexOf = function(needle) {
+        var i = -1,
+          index = -1;
+
+        for (i = 0; i < this.length; i++) {
+          var item = this[i];
+
+          if ((findNaN && item !== item) || item === needle) {
+            index = i;
+            break;
+          }
+        }
+
+        return index;
+      };
+    }
+
+    return indexOf.call(this, needle) > -1;
+  };
 };
